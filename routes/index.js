97f017f03003a8eaddf5be77ddb37903082ce52a
Francisco <you@example.com>
2025-10-29 14:24:01 +0100

    Refactor card and gallery components in index.ejs for better layout; update styles in components.css and style.css to enhance visual presentation and responsiveness.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var dataService = require("../data/dataService")
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  const items = dataService.findAllProductosLessThan(50);
+  res.render('index', { productos : items });
+});
+
+router.get("/producto/:id",function(req,res,next){
+  const item = ( dataService.findProductoById( req.params.id))
+  res.render('item',{ producto : item })
 });
 
 module.exports = router;
