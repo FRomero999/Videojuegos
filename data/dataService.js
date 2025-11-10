319c9caac5f08d80420846a38706a1116997f8a0
Francisco <you@example.com>
2025-11-10 08:04:36 +0100

    Add saveProductosTienda function to dataService.js for persisting product data; extend productos.json with new game entries including Super Mario Odyssey, Horizon Zero Dawn, Minecraft, Elden Ring, and Stardew Valley, enhancing the product catalog.

--- data/dataService.js
+++ data/dataService.js
@@ -5,6 +5,14 @@ const productosTienda = JSON.parse(
     fs.readFileSync(path.join(__dirname, 'productos.json'), 'utf8')
 );
 
+function saveProductosTienda() {
+    fs.writeFileSync(
+        path.join(__dirname, 'productos.json'),
+        JSON.stringify(productosTienda),
+        'utf8'
+    );
+}
+
 function findAllProductos() {
     return productosTienda;
 }
@@ -13,10 +21,10 @@ function findAllProductosLessThan(precio) {
     return productosTienda.filter((e) => { return (e.precio < precio) })
 }
 
-function findProductoById(id) {
-    
+function findProductoById(id) {   
     return productosTienda.filter((e) => { return (e.id == id) })[0]
 }
+
 
 
 module.exports = {
