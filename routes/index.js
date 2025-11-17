6b6cf03714c5cd472330462b5929ba94fab13ee3
Francisco <you@example.com>
2025-11-17 09:03:22 +0100

    Implement product deletion functionality in admin interface; update routes to handle delete requests and modify admin.ejs to dynamically display products with delete buttons, enhancing product management experience.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var dataService = require("../data/dataService")
+var dataService = require("../data/dataService");
+const { get } = require('.');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,8 +15,41 @@ router.get("/producto/:pid",function(req,res,next){
   res.render('item',{ item: item })
 });
 
+/*
+router.get("/admin/edit/:pid",function(req,res,next){
+  const item = ( dataService.findProductoById( req.params.pid))
+  console.log(item)
+  res.render('edit-item',{ item: item })
+});
+*/
+
+router.get("/admin/producto/:pid",function(req,res,next){
+  const id = req.params.pid
+  const option = req.query.operation
+  if(dataService.deleteProductoById(id)){
+    res.redirect("/admin")
+  }
+  else res.redirect("/admin")
+  
+});
+
+/* router.post("/admin/producto/:pid/delete",function(req,res,next){
+  const id = req.params.pid
+  dataService.deleteProductoById(id)
+  res.redirect("/admin")  
+}); */
+
+
+router.post("/admin/producto/delete",function(req,res,next){
+    
+  const id = req.body.id
+  dataService.deleteProductoById(id)
+  res.redirect("/admin")  
+});
+
+
 router.get("/admin",function(req,res){
-  res.render("admin",{})
+  res.render("admin",{ productos : dataService.findAllProductos()})
 });
 
 module.exports = router;
